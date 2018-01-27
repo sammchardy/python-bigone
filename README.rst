79c9b9cd8f704da874c5adf95b66714f7103c345
Sam McHardy <hello@sammchardy.net>
2018-01-28 07:30:12 +1100

    Update to v0.0.4

--- README.rst
+++ README.rst
@@ -1,5 +1,5 @@
 ===============================
-Welcome to python-bigone v0.0.3
+Welcome to python-bigone v0.0.4
 ===============================
 
 .. image:: https://img.shields.io/pypi/v/python-bigone.svg
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='python-bigone',
-    version='0.0.3',
+    version='0.0.4',
     packages=['bigone'],
     description='BigONE REST API python implementation',
     url='https://github.com/sammchardy/python-bigone',
