63d74b9e9e632356f0990fc7990c38272b413cb0
Sam McHardy <hello@sammchardy.net>
2018-06-27 19:21:43 +1000

    Fetch version number from init.py

--- setup.py
+++ setup.py
@@ -1,9 +1,32 @@
 #!/usr/bin/env python
+
+import codecs
+import os
+import re
+
 from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    with codecs.open(os.path.join(here, *parts), 'r') as fp:
+        return \
+            fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
 
 setup(
     name='python-bigone',
-    version='0.1.0',
+    version=find_version("bigone", "__init__.py"),
     packages=['bigone'],
     description='BigONE REST API python implementation',
     url='https://github.com/sammchardy/python-bigone',
